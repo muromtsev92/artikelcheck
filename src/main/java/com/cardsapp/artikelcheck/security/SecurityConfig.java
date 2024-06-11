f48e5bdf3b5adf440dd9a2bcd0c315399bfa69fd
Sergei Muromtsev <k29393@yandex.ru>
2024-06-11 16:46:52 +0500

    spring configuration

--- src/main/java/com/cardsapp/artikelcheck/security/SecurityConfig.java
+++ src/main/java/com/cardsapp/artikelcheck/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.cardsapp.artikelcheck.security;
 
+import com.cardsapp.artikelcheck.model.users.User;
+import com.cardsapp.artikelcheck.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -17,22 +19,31 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        List<UserDetails> usersList = new ArrayList<>();
+//        usersList.add(new User(
+//                "buzz", encoder.encode("password"),
+//                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+//        usersList.add(new User(
+//                "woody", encoder.encode("password"),
+//                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+//        return new InMemoryUserDetailsManager(usersList);
+//    }
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(new User(
-                "buzz", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(new User(
-                "woody", encoder.encode("password"),
-                Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
-        return new InMemoryUserDetailsManager(usersList);
-    }
 
 
 }
