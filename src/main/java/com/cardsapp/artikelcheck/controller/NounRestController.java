fc22356babbc1c767d4a229eae264c6e9bb3f078
Sergei Muromtsev <k29393@yandex.ru>
2024-07-24 17:34:38 +0500

    add rest controller for random words

---
+++ src/main/java/com/cardsapp/artikelcheck/controller/NounRestController.java
@@ -0,0 +1,27 @@
+package com.cardsapp.artikelcheck.controller;
+
+import com.cardsapp.artikelcheck.dto.NounDto;
+import com.cardsapp.artikelcheck.model.words.Noun;
+import com.cardsapp.artikelcheck.service.WordService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nouns")
+@Slf4j
+@RequiredArgsConstructor
+public class NounRestController {
+    private final WordService<NounDto> nounService;
+
+    @GetMapping("/get-rnd")
+    public List<NounDto> getNounsForGame(){
+        log.info("rest get mapping");
+        return nounService.getRandomWords(3);
+    }
+}
